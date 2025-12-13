5a72f81255aa3f2405c9b8938f2d899b4f63c9ae
DuyBao1001 <24520190@gm.uit.edu.vn>
2025-12-13 16:56:40 +0700

    hash256 password

---
+++ NewsApp/Common/HashHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NewsApp.Common
+{
+    public static class HashHelper
+    {
+        public static string Hash(string plainText)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Chuyển chuỗi thành mảng byte
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+
+                // Chuyển mảng byte ngược lại thành chuỗi Hex (hệ 16)
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
