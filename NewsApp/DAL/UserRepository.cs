2de9dc5b41a68a9e76d56c734c692458a2a8f6c6
24520372-DuyHo <24520372@gm.uit.edu.vn>
2025-12-17 15:21:00 +0700

    Update UserRepository.cs
    update CheckUserNameExists, GetAll, GetByID, Login

--- NewsApp/DAL/UserRepository.cs
+++ NewsApp/DAL/UserRepository.cs
@@ -1,3 +1,95 @@
 using Microsoft.Data.SqlClient;
 using NewsApp.Data;
 
+namespace NewsApp.DAL
+{
+    internal class AccountRepository : BaseRepository<Account>, IAccountRepository
+    {
+        public AccountRepository() : base()
+        {
+        }
+
+        public bool CheckUserNameExists(string userName)
+        {
+            try
+            {
+                using SqlConnection connection = GetConnection();
+                connection.Open();
+                string query = "SELECT UserName FROM Account WHERE UserName = @userName";
+                SqlCommand command = new(query, connection);
+                command.Parameters.AddWithValue("@userName", userName);
+                SqlDataReader reader = command.ExecuteReader();
+                return reader.HasRows;
+            }
+            catch (SqlException sqle)
+            {
+                throw new NotImplementedException(sqle.Message);
+            }
+        }
+
+
+
+        public override bool Delete(Account obj)
+        {
+            //try
+            //{
+            //    using SqlConnection connection = GetConnection();
+            //    connection.Open();
+            //    string query = "DELETE FROM Account WHERE AccountID = @accountId";
+            //    SqlCommand command = new(query, connection);
+            //    command.Parameters.AddWithValue("@accountId", obj.AccountID);
+            //    return command.ExecuteNonQuery() > 0;
+            //}
+            //catch (SqlException sqle)
+            //{
+            //    Console.WriteLine(sqle.Message);
+            //    return false;
+            //}
+            throw new NotImplementedException();
+        }
+
+        public override IList<Account> GetAll()
+        {
+            try
+            {
+                using SqlConnection connection = GetConnection();
+                connection.Open();
+                string query = "SELECT * FROM Account";
+                SqlCommand command = new(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                return [];
+            }
+            catch (SqlException sqle)
+            {
+                Console.WriteLine(sqle.Message);
+                return [];
+            }
+        }
+
+        public override Account? GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(Account account)
+        {
+            try
+            {
+                using SqlConnection connection = GetConnection();
+                String query = "SELECT UserName FROM Account WHERE UserName = @userName AND Password = @password";
+                SqlCommand command = new(query, connection);
+                command.Parameters.AddWithValue("@userName", account.Username);
+                command.Parameters.AddWithValue("@password", account.Password);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                return reader.HasRows;
+            }
+            catch (SqlException sqle)
+            {
+                Console.WriteLine(sqle.Message);
+                return false;
+            }
+        }
+
+    }
+}
