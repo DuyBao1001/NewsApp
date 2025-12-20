6d39d172fab599c72370b3eeddc5588db3f60298
24521320-ThuanPhat <24521320@gm.uit.edu.vn>
2025-12-20 13:35:19 +0700

    Up them Implement Category management

--- NewsApp/BLL/AccountServices.cs
+++ NewsApp/BLL/AccountServices.cs
@@ -1,12 +1,194 @@
-
+﻿
 using NewsApp.Common;
 using NewsApp.Data;
 using NewsApp.Network;
-using System.Text.Json;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text.Json;
 
 namespace NewsApp.BLL
 {
 
-    
+    public class AccountServices
+    {
+        private readonly ClientSocket _socketClient;
+
+        // Kết quả đăng ký
+        public bool RegisterProcessing { get; set; }
+
+        // User đã đăng nhập
+        public User? UserAuthenticated { get; private set; }
+
+        // Khai báo các sự kiện
+        public event Action<User?, string>? LoginResult;
+        public event Action<bool, string>? RegisterResult;
+        public event Action<string>? ConnectionStatusChanged;
+        public event Action<Packet, string>? ReceivedData;
+        public event Action<string>? ErrorData;
+        public event Action<bool, string>? UpdateProfileResult;
+        public event Action<bool, string>? ChangePasswordResult;
+
+        public bool IsConnected { get { return _socketClient != null && _socketClient.IsConnected; } }
+
+
+
+        public AccountServices()
+        {
+            _socketClient = new ClientSocket();
+            _socketClient.DataReceive += HandleDataReceive;
+            _socketClient.Start();
+        }
+
+        private void HandleDataReceive(Packet? packet)
+        {
+            if (packet == null)
+            {
+                ErrorData?.Invoke("Dữ liệu nhận được không hợp lệ");
+                return;
+            }
+
+            string command = packet.Command;
+            string payload = packet.Payload;
+
+            switch (command)
+            {
+                case MessageProtocol.ResponseCommand.LOGIN_SUCCESS:
+                    {
+                        User? user = JsonSerializer.Deserialize<User>(payload);
+                        UserAuthenticated = user;
+                        LoginResult?.Invoke(user, "Đăng nhập thành công");
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.LOGIN_FAIL:
+                    {
+                        string errorMessage = JsonSerializer.Deserialize<string>(payload) ?? "Đăng nhập thất bại";
+                        LoginResult?.Invoke(null, errorMessage);
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.REGISTER_SUCCESS:
+                    {
+                        RegisterResult?.Invoke(true, "Đăng ký thành công");
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.REGISTER_FAIL:
+                    {
+                        RegisterResult?.Invoke(false, "Đăng ký thất bại");
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.UPDATE_PROFILE_SUCCESS:
+                    UpdateProfileResult?.Invoke(true, "Cập nhật thông tin thành công!");
+                    break;
+
+                case MessageProtocol.ResponseCommand.UPDATE_PROFILE_FAIL:
+                    UpdateProfileResult?.Invoke(false, "Cập nhật thất bại: " + packet.Payload);
+                    break;
+                case MessageProtocol.ResponseCommand.CHANGE_PASSWORD_SUCCESS:
+                    ChangePasswordResult?.Invoke(true, "Đổi mật khẩu thành công!");
+                    break;
+
+                case MessageProtocol.ResponseCommand.CHANGE_PASSWORD_FAIL:
+                    string msg = JsonSerializer.Deserialize<string>(payload) ?? "Đổi mật khẩu thất bại";
+                    ChangePasswordResult?.Invoke(false, msg);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void HandleLoginResult(User? packet, string command)
+        {
+            LoginResult?.Invoke(packet, command);
+        }
+
+        private void HandleRegisterResult(bool result, string command)
+        {
+            RegisterResult?.Invoke(result, command);
+        }
+
+        private void HandleErrorData(string message)
+        {
+            ErrorData?.Invoke(message);
+        }
+
+        private void HandleConnectionStatusChanged(string message)
+        {
+            ConnectionStatusChanged?.Invoke(message);
+        }
+
+        public void Login(string userName, string password)
+        {
+            if (IsConnected)
+            {
+                Account account = new()
+                {
+                    Username = userName,
+                    Password = password,
+                };
+                Packet requestPacket = new(MessageProtocol.RequestCommand.LOGIN, account);
+                _socketClient.SendRequest(requestPacket);
+            }
+        }
+
+        public void Register(string userName, string password, string email, DateTime birthDay, string fullName, string role)
+        {
+            if (IsConnected)
+            {
+                RegisterModel registerModel = new()
+                {
+                    Username = userName,
+                    Password = password,
+                    Email = email,
+                    BirthDay = birthDay,
+                    FullName = fullName,
+                    Role = role
+                };
+                Packet requestPacket = new(MessageProtocol.RequestCommand.REGISTER, registerModel);
+                _socketClient.SendRequest(requestPacket);
+            }
+        }
+
+        public void Logout()
+        {
+            if (IsConnected)
+            {
+                Packet requestPacket = new(MessageProtocol.RequestCommand.LOGOUT, "");
+                _socketClient.SendRequest(requestPacket);
+            }
+        }
+
+        public void UpdateProfile(User user)
+        {
+            if (IsConnected)
+            {
+                // Serialize User thành JSON (bao gồm cả Avatar byte[])
+                string payload = JsonSerializer.Serialize(user);
+
+                Packet request = new Packet(
+                    MessageProtocol.RequestCommand.UPDATE_PROFILE,
+                    payload
+                );
+
+                _socketClient.SendRequest(request);
+            }
+        }
+
+        public void ChangePassword(string username, string oldPass, string newPass)
+        {
+            if (IsConnected)
+            {
+
+                ChangePasswordModel model = new ChangePasswordModel
+                {
+                    Username = username,
+                    OldPassword = oldPass,
+                    NewPassword = newPass
+                };
+
+                string payload = JsonSerializer.Serialize(model);
+                Packet request = new Packet(MessageProtocol.RequestCommand.CHANGE_PASSWORD, payload);
+
+                _socketClient.SendRequest(request);
+            }
+        }
+    }
 }
--- NewsApp/BLL/ArticleServices.cs
+++ NewsApp/BLL/ArticleServices.cs
@@ -1,17 +1,222 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging.Abstractions;
+using NewsApp.Common;
+using NewsApp.Data;
+using NewsApp.Network;
 using System.ComponentModel;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Runtime.Intrinsics.Wasm;
 using System.Runtime.Intrinsics.X86;
 using System.Security.Policy;
 using System.Text.Json;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Logging.Abstractions;
-using NewsApp.Common;
-using NewsApp.Data;
-using NewsApp.Network;
 
 namespace NewsApp.BLL
 {
-    
+    public class ArticleServices
+    {
+        private readonly ClientSocket _clientSocket;
+
+        public bool IsConnected
+        {
+            get { return _clientSocket != null && _clientSocket.IsConnected; }
+        }
+
+        public event Action<IList<Article>?>? ListArticleChanged;
+
+        public event Action<Article?>? ArticleDetailChanged;
+
+        public event Action<string>? ErrorData;
+
+        public event Action<bool, string>? DataChanged;
+
+        public event Action<string> ResponseError;
+
+        public event Action<string>? ConnectionStatusChanged;
+
+        public event Action<List<Article>>? GetLatestArticlesResult;
+        public event Action<List<Article>>? GetArticlesByCategoryResult;
+        public event Action<List<Article>>? SearchArticlesResult;
+        public event Action<string>? NotificationReceived;
+
+        public ArticleServices()
+        {
+            _clientSocket = new ClientSocket();
+            _clientSocket.Start();
+
+            _clientSocket.DataReceive += HandleDataReceive;
+            _clientSocket.ConnectionStatusChanged += HandleConnectionStatusChanged;
+            _clientSocket.ErrorData += HandleErrorData;
+        }
+
+        private void HandleDataReceive(Packet? packet)
+        {
+            if (packet == null)
+            {
+                ResponseError?.Invoke("Không nhận được phản hồi từ server");
+                return;
+            }
+
+            string command = packet.Command;
+            string payload = packet.Payload;
+
+            switch (command)
+            {
+                case MessageProtocol.ResponseCommand.GET_LIST_ARTICLES_SUCCESS:
+                    {
+                        IList<Article>? articles = JsonSerializer.Deserialize<IList<Article>>(
+                            payload
+                        );
+                        ListArticleChanged?.Invoke(articles);
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.GET_LIST_ARTICLES_FAIL:
+                    ResponseError?.Invoke("Không thể lấy danh sách bài viết");
+                    break;
+                case MessageProtocol.ResponseCommand.GET_ARTICLE_DETAIL_SUCCESS:
+                    {
+                        Article? article = JsonSerializer.Deserialize<Article>(payload);
+                        ArticleDetailChanged?.Invoke(article);
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.GET_ARTICLE_DETAIL_FAIL:
+                    ResponseError?.Invoke("Không thể lấy chi tiết bài viết");
+                    break;
+                case MessageProtocol.ResponseCommand.POST_ARTICLE_SUCCESS:
+                    DataChanged?.Invoke(true, "Đăng bài viết đã được đăng lên hệ thống, hãy chờ duyệt");
+                    break;
+                case MessageProtocol.ResponseCommand.POST_ARTICLE_FAIL:
+                    DataChanged?.Invoke(false, "Đăng bài viết thất bại");
+                    break;
+                case MessageProtocol.ResponseCommand.GET_LATEST_ARTICLES_SUCCESS:
+                    {
+                        var articles = JsonSerializer.Deserialize<List<Article>>(payload);
+                        GetLatestArticlesResult?.Invoke(articles);
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.GET_LATEST_ARTICLES_FAIL:
+                    break;
+                case MessageProtocol.ResponseCommand.GET_ARTICLES_BY_CATEGORY_SUCCESS:
+                    {
+                        var articles = JsonSerializer.Deserialize<List<Article>>(payload);
+                        GetArticlesByCategoryResult?.Invoke(articles);
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.GET_ARTICLES_BY_CATEGORY_FAIL:
+                    break;
+                case MessageProtocol.ResponseCommand.SEARCH_ARTICLES_SUCCESS:
+                    {
+                        var articles = JsonSerializer.Deserialize<List<Article>>(payload);
+                        SearchArticlesResult?.Invoke(articles);
+                    }
+                    break;
+                case MessageProtocol.ResponseCommand.SEARCH_ARTICLES_FAIL:
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // Methods để gửi requests
+        public void GetListArticles()
+        {
+            if (IsConnected)
+            {
+                Packet requestPacket = new(
+                    MessageProtocol.RequestCommand.GET_LIST_ARTICLES,
+                    new { }
+                );
+                _clientSocket.SendRequest(requestPacket);
+            }
+        }
+
+        public void GetArticleDetail(int articleId)
+        {
+            if (IsConnected)
+            {
+                Packet requestPacket = new(
+                    MessageProtocol.RequestCommand.GET_ARTICLE_DETAIL,
+                    new { articleId }
+                );
+                _clientSocket.SendRequest(requestPacket);
+            }
+        }
+
+        public void GetLatestArticles()
+        {
+            if (IsConnected)
+            {
+                Packet requestPacket = new(
+                    MessageProtocol.RequestCommand.GET_LATEST_ARTICLES,
+                    null
+                );
+                _clientSocket.SendRequest(requestPacket);
+            }
+        }
+
+        public void GetArticlesByCategory(int categoryId)
+        {
+            if (IsConnected)
+            {
+                Packet requestPacket = new(
+                    MessageProtocol.RequestCommand.GET_ARTICLES_BY_CATEGORY,
+                    categoryId
+                );
+                _clientSocket.SendRequest(requestPacket);
+            }
+        }
+
+        public void SearchArticles(string keyword)
+        {
+            if (IsConnected)
+            {
+                Packet requestPacket = new(MessageProtocol.RequestCommand.SEARCH_ARTICLES, keyword);
+                _clientSocket.SendRequest(requestPacket);
+            }
+        }
+
+        public void PostArticle(
+            string title,
+            int categoryId,
+            string categoryName,
+            string content,
+            byte[]? image,
+            int authorId,
+            string authorName
+        )
+        {
+            var articleData = new
+            {
+                Title = title,
+                CategoryID = categoryId,
+                CategoryName = categoryName,
+                Content = content,
+                Image = image,
+                AuthorID = authorId,
+                AuthorName = authorName,
+                PublishDate = DateTime.Now,
+            };
+
+            string payload = JsonSerializer.Serialize(articleData);
+            Packet packet = new Packet
+            {
+                Command = MessageProtocol.RequestCommand.POST_ARTICLE,
+                Payload = payload,
+            };
+            _clientSocket.SendRequest(packet);
+        }
+
+        private void HandleConnectionStatusChanged(string message)
+        {
+            ConnectionStatusChanged?.Invoke(message);
+        }
+
+        private void HandleErrorData(string message)
+        {
+            ErrorData?.Invoke(message);
+        }
+    }
 }
