b83fa678343248c7d4f965fe49fa7ce0fc7d4887
DuyBao1001 <24520190@gm.uit.edu.vn>
2025-12-20 12:04:48 +0700

    Sửa common user và comment + thêm ChangePassForm + done các form còn lại

--- NewsApp/Data/User.cs
+++ NewsApp/Data/User.cs
@@ -10,12 +10,13 @@ namespace NewsApp.Data
 {
     public class User
     {
-        public int Id { get; set;}
+        public int Id { get; set; }
         public required string FullName { get; set; }
-        public required string Email { get; set;}
+        public required string Email { get; set; }
         public DateTime BirthDay { get; set; }
         public int AccountID { get; set; }
-        public required string UserName { get; set;}
+        public required string UserName { get; set; }
         public string Role { get; set; } = "Reader";
+        public byte[]? Avatar { get; set; }
     }
 }
