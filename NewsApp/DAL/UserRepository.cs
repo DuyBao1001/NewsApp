b7c5141c3d928d2264467bda15db601b4038810c
24520372-DuyHo <24520372@gm.uit.edu.vn>
2025-12-20 17:44:02 +0700

    Update UserRepository.cs

--- NewsApp/DAL/UserRepository.cs
+++ NewsApp/DAL/UserRepository.cs
@@ -1,88 +1,42 @@
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 using NewsApp.Data;
+using System.Data;
 
 namespace NewsApp.DAL
 {
-    internal class AccountRepository : BaseRepository<Account>, IAccountRepository
+    public class UserRepository : BaseRepository<User>
     {
-        public AccountRepository() : base()
+        public UserRepository() : base()
         {
         }
 
-        public bool CheckUserNameExists(string userName)
+        public override bool Save(User obj)
         {
             try
             {
                 using SqlConnection connection = GetConnection();
-                connection.Open();
-                string query = "SELECT UserName FROM Account WHERE UserName = @userName";
+                // Thêm cột Avatar vào câu lệnh INSERT
+                string query = "INSERT INTO [User] (FullName, Email, BirthDay, Role, AccountID, Avatar) VALUES (@fullName, @email, @birthDay, @role, @accountId, @avatar)";
+
                 SqlCommand command = new(query, connection);
-                command.Parameters.AddWithValue("@userName", userName);
-                SqlDataReader reader = command.ExecuteReader();
-                return reader.HasRows;
-            }
-            catch (SqlException sqle)
-            {
-                throw new NotImplementedException(sqle.Message);
-            }
-        }
+                command.Parameters.AddWithValue("@fullName", obj.FullName);
+                command.Parameters.AddWithValue("@email", obj.Email);
+                command.Parameters.AddWithValue("@birthDay", obj.BirthDay);
+                command.Parameters.AddWithValue("@role", obj.Role);
+                command.Parameters.AddWithValue("@accountId", obj.AccountID);
 
+                // Xử lý tham số Avatar (có thể null)
+                if (obj.Avatar == null)
+                {
+                    command.Parameters.Add("@avatar", SqlDbType.VarBinary, -1).Value = DBNull.Value;
+                }
+                else
+                {
+                    command.Parameters.AddWithValue("@avatar", obj.Avatar);
+                }
 
-
-        public override bool Delete(Account obj)
-        {
-            //try
-            //{
-            //    using SqlConnection connection = GetConnection();
-            //    connection.Open();
-            //    string query = "DELETE FROM Account WHERE AccountID = @accountId";
-            //    SqlCommand command = new(query, connection);
-            //    command.Parameters.AddWithValue("@accountId", obj.AccountID);
-            //    return command.ExecuteNonQuery() > 0;
-            //}
-            //catch (SqlException sqle)
-            //{
-            //    Console.WriteLine(sqle.Message);
-            //    return false;
-            //}
-            throw new NotImplementedException();
-        }
-
-        public override IList<Account> GetAll()
-        {
-            try
-            {
-                using SqlConnection connection = GetConnection();
                 connection.Open();
-                string query = "SELECT * FROM Account";
-                SqlCommand command = new(query, connection);
-                SqlDataReader reader = command.ExecuteReader();
-                return [];
-            }
-            catch (SqlException sqle)
-            {
-                Console.WriteLine(sqle.Message);
-                return [];
-            }
-        }
-
-        public override Account? GetByID(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Login(Account account)
-        {
-            try
-            {
-                using SqlConnection connection = GetConnection();
-                String query = "SELECT UserName FROM Account WHERE UserName = @userName AND Password = @password";
-                SqlCommand command = new(query, connection);
-                command.Parameters.AddWithValue("@userName", account.Username);
-                command.Parameters.AddWithValue("@password", account.Password);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                return reader.HasRows;
+                return command.ExecuteNonQuery() > 0;
             }
             catch (SqlException sqle)
             {
@@ -91,5 +45,220 @@ namespace NewsApp.DAL
             }
         }
 
+        // Đã hiện thực hàm Update để cập nhật thông tin và Avatar
+        public override bool Update(User obj)
+        {
+            try
+            {
+                using (SqlConnection connection = GetConnection())
+                {
+                    connection.Open();
+                    string query = @"UPDATE [User] 
+                                     SET FullName = @fullName, 
+                                         Email = @email, 
+                                         BirthDay = @birthDay,
+                                         Avatar = @avatar
+                                     WHERE UserID = @id";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@fullName", obj.FullName);
+                    command.Parameters.AddWithValue("@email", obj.Email);
+                    command.Parameters.AddWithValue("@birthDay", obj.BirthDay);
+                    command.Parameters.AddWithValue("@id", obj.Id);
+
+                    // Xử lý tham số Avatar
+                    if (obj.Avatar == null)
+                    {
+                        command.Parameters.Add("@avatar", SqlDbType.VarBinary, -1).Value = DBNull.Value;
+                    }
+                    else
+                    {
+                        command.Parameters.AddWithValue("@avatar", obj.Avatar);
+                    }
+
+                    return command.ExecuteNonQuery() > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi Update User: " + ex.Message);
+                return false;
+            }
+        }
+
+        public override bool Delete(User obj)
+        {
+            try
+            {
+                using (SqlConnection connection = GetConnection())
+                {
+                    connection.Open();
+
+                    int accountIdToDelete = obj.AccountID;
+
+                    if (accountIdToDelete == 0)
+                    {
+                        string findAccountQuery = "SELECT AccountID FROM [User] WHERE UserID = @userId";
+                        using (SqlCommand findCmd = new SqlCommand(findAccountQuery, connection))
+                        {
+                            findCmd.Parameters.AddWithValue("@userId", obj.Id);
+                            object result = findCmd.ExecuteScalar();
+                            if (result != null && result != DBNull.Value)
+                            {
+                                accountIdToDelete = Convert.ToInt32(result);
+                            }
+                        }
+                    }
+
+                    if (accountIdToDelete == 0) return false;
+
+                    using (SqlTransaction tran = connection.BeginTransaction())
+                    {
+                        try
+                        {
+                            // Bước 1: Xóa tất cả Comment của User này trước
+                            string queryDelComment = "DELETE FROM [Comment] WHERE UserID = @userId";
+                            SqlCommand commandDelComment = new SqlCommand(queryDelComment, connection, tran);
+                            commandDelComment.Parameters.AddWithValue("@userId", obj.Id);
+                            commandDelComment.ExecuteNonQuery();
+
+                            // Bước 2: Sau đó mới xóa User
+                            string queryUser = "DELETE FROM [User] WHERE UserID = @userId";
+                            SqlCommand commandUser = new SqlCommand(queryUser, connection, tran);
+                            commandUser.Parameters.AddWithValue("@userId", obj.Id);
+                            commandUser.ExecuteNonQuery();
+
+                            // Bước 3: Cuối cùng xóa Account
+                            string queryAcc = "DELETE FROM Account WHERE AccountID = @accountId";
+                            SqlCommand commandAcc = new SqlCommand(queryAcc, connection, tran);
+                            commandAcc.Parameters.AddWithValue("@accountId", accountIdToDelete);
+                            int rows = commandAcc.ExecuteNonQuery();
+
+                            tran.Commit();
+                            return rows > 0;
+                        }
+                        catch (Exception)
+                        {
+                            tran.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqle)
+            {
+                Console.WriteLine(sqle.Message);
+                LastError = sqle.Message;
+                return false;
+            }
+        }
+
+        public override IList<User> GetAll()
+        {
+            List<User> users = new List<User>();
+            try
+            {
+                using SqlConnection connection = GetConnection();
+                // Thêm U.Avatar vào SELECT
+                string query = "SELECT U.UserID, FullName, Email, BirthDay, Role, U.AccountID, A.UserName, U.Avatar FROM [User] as U JOIN Account as A ON U.AccountID = A.AccountID";
+                SqlCommand command = new(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    users.Add(new User
+                    {
+                        Id = reader.GetInt32(0),
+                        FullName = reader.GetString(1),
+                        Email = reader.GetString(2),
+                        BirthDay = reader.GetDateTime(3),
+                        Role = reader.GetString(4),
+                        AccountID = reader.GetInt32(5),
+                        UserName = reader.GetString(6),
+                        // Đọc Avatar (cột thứ 7 - index 7 vì bắt đầu từ 0)
+                        Avatar = reader.IsDBNull(7) ? null : (byte[])reader[7]
+                    });
+                }
+            }
+            catch (SqlException sqle)
+            {
+                Console.WriteLine(sqle.Message);
+                LastError = sqle.Message;
+            }
+            return users;
+        }
+
+        public User? GetByAccountID(int accountId)
+        {
+            try
+            {
+                using SqlConnection connection = GetConnection();
+                // Thêm U.Avatar vào SELECT
+                string query = "SELECT U.UserID, FullName, Email, BirthDay, Role, U.AccountID, A.UserName, U.Avatar FROM [User] as U JOIN Account as A ON U.AccountID = A.AccountID WHERE U.AccountID = @accountId";
+                SqlCommand command = new(query, connection);
+                command.Parameters.AddWithValue("@accountId", accountId);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return new User
+                    {
+                        Id = reader.GetInt32(0),
+                        FullName = reader.GetString(1),
+                        Email = reader.GetString(2),
+                        BirthDay = reader.GetDateTime(3),
+                        Role = reader.GetString(4),
+                        AccountID = reader.GetInt32(5),
+                        UserName = reader.GetString(6),
+                        // Đọc Avatar
+                        Avatar = reader.IsDBNull(7) ? null : (byte[])reader[7]
+                    };
+                }
+                return null;
+            }
+            catch (SqlException sqle)
+            {
+                Console.WriteLine(sqle.Message);
+                return null;
+            }
+        }
+
+        public override User? GetByID(int id)
+        {
+            try
+            {
+                using (SqlConnection connection = GetConnection())
+                {
+                    // Thêm U.Avatar vào SELECT
+                    String query = "SELECT U.UserID, FullName, Email, BirthDay, Role, U.AccountID, A.UserName, U.Avatar FROM [User] as U JOIN Account as A ON U.AccountID = A.AccountID WHERE U.UserID = @userId";
+                    SqlCommand command = new(query, connection);
+                    command.Parameters.AddWithValue("@userId", id);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        User user = new()
+                        {
+                            Id = reader.GetInt32(0),
+                            FullName = reader.GetString(1),
+                            Email = reader.GetString(2),
+                            BirthDay = reader.GetDateTime(3),
+                            Role = reader.GetString(4),
+                            AccountID = reader.GetInt32(5),
+                            UserName = reader.GetString(6),
+                            // Đọc Avatar
+                            Avatar = reader.IsDBNull(7) ? null : (byte[])reader[7]
+                        };
+                        return user;
+                    }
+                }
+                return null;
+            }
+            catch (SqlException sqle)
+            {
+                Console.WriteLine(sqle.Message);
+                return null;
+            }
+        }
     }
 }
