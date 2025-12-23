cf989af844674c38b4bcbb378eefde7205e9a6f8
24520372-DuyHo <24520372@gm.uit.edu.vn>
2025-12-23 23:41:54 +0700

    Update OtpManager.cs

---
+++ NewsApp/BLL/OtpManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace NewsApp.BLL
+{
+    public static class OtpManager
+    {
+        private static Dictionary<string, string> _otpStore = new Dictionary<string, string>();
+
+        public static void SaveOtp(string email, string otp)
+        {
+            if (_otpStore.ContainsKey(email))
+            {
+                _otpStore[email] = otp;
+            }
+            else
+            {
+                _otpStore.Add(email, otp);
+            }
+        }
+
+        public static bool VerifyOtp(string email, string otpInput)
+        {
+            if (_otpStore.ContainsKey(email))
+            {
+                if (_otpStore[email] == otpInput)
+                {
+                    _otpStore.Remove(email);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
