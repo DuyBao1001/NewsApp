69b8f74c75f49278e4bd5c063ed3271addab0f45
DuyBao1001 <24520190@gm.uit.edu.vn>
2025-12-24 09:23:07 +0700

    cập nhật database helper

--- NewsApp/DAL/DatabaseHepper.cs
+++ NewsApp/DAL/DatabaseHepper.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace NewsApp.DAL
 {
@@ -15,12 +13,86 @@ namespace NewsApp.DAL
 
         static DatabaseHepper()
         {
-            var builder = new ConfigurationBuilder();
-            builder.SetBasePath(AppContext.BaseDirectory).
-                AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-            _configuration = builder.Build();
+            try
+            {
+                var builder = new ConfigurationBuilder();
+                builder.SetBasePath(AppContext.BaseDirectory)
+                       .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                _configuration = builder.Build();
+            }
+            catch
+            {
+
+            }
         }
 
-        public static string ConnectionString => _configuration.GetConnectionString("DefaultConnection") ?? DEFAULT_CONNECTION;
+        public static string ConnectionString
+        {
+            get
+            {
+                if (_configuration != null)
+                {
+                    return _configuration.GetConnectionString("DefaultConnection") ?? DEFAULT_CONNECTION;
+                }
+                return DEFAULT_CONNECTION;
+            }
+        }
+
+        public static int ExecuteNonQuery(string query, SqlParameter[] parameters = null)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        if (parameters != null) command.Parameters.AddRange(parameters);
+                        return command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("LỖI DATABASE RỒI: " + ex.Message);
+                return -1;
+            }
+        }
+        public static object ExecuteScalar(string query, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+                    return command.ExecuteScalar();
+                }
+            }
+        }
+        public static DataTable GetDataTable(string query, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    DataTable dt = new DataTable();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dt);
+                    }
+                    return dt;
+                }
+            }
+        }
     }
 }
